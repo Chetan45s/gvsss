ca8706876debed2fbe12ddf3e3360da5fbf5073a
Chetan Salmotra <chetansalmotra45@gmail.com>
2021-06-29 02:00:59 +0530

    adding template

---
+++ gvsss/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for gvsss project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gvsss.settings')
+
+application = get_asgi_application()
