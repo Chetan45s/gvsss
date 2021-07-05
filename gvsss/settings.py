672dbc189a4cdbebdd3bb38eb1ef361de2372e07
Chetan45s <chetansalmotra45@gmail.com>
2021-07-05 20:49:13 +0530

    add

--- gvsss/settings.py
+++ gvsss/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'main',
 ]
-
+ 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
